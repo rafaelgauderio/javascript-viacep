8a83c88890e091205de13e346bd88d86fb9073e5
Rafael de Luca <deluca1712@gmail.com>
2022-12-15 13:22:21 -0300

    funcao construtora com todos os campos do formulario

--- js/controllers/controlador-formulario.js
+++ js/controllers/controlador-formulario.js
@@ -1,11 +1,45 @@
 // controladores em MCV são os módulos, componentes que tratam os eventos do usuário
 
+
+//função construtora
+function State () {
+    this.botaoSalvar = null;
+    this.botaoLimpar = null;
+
+    this.inputCep= null;
+    this.inputLogradouro = null;
+    this.inputNumero = null;
+    this.inputCity = null;
+
+    this.erroCep = null;
+    this.erroNumbero = null;
+}
+
+// instanciando um objeto State
+const state = new State();
+
+
+
+
 export function init() {
 
     // função responsável por iniciar o controlador
 
-    const inputCep = document.forms.enderecoNovo.cep;
-    console.log(inputCep);
+    //inputs
+    state.inputCep = document.forms.enderecoNovo.cep;
+    state.inputLogradouro = document.forms.enderecoNovo.logradouro;
+    state.inputNumero = document.forms.enderecoNovo.numero;
+    state.inputCidade = document.forms.enderecoNovo.cidade;
+
+    //botoes
+    state.botaoSalvar = document.forms.enderecoNovo.botaoSalvar;
+    state.botaoSalvar = document.forms.enderecoNovo.botaoLimpar;
+
+    //erros
+    state.erroCep = document.querySelector('[data-erro="cep"]');
+    state.erroNumero = document.querySelector('[data-erro="numero"]');
+
+    console.log(state);
 
 
 }
