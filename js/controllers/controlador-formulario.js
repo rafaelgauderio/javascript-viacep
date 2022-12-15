f3180dcda34225706423bbec4d95c949ae2a8641
Rafael de Luca <deluca1712@gmail.com>
2022-12-15 13:04:52 -0300

    Iniciando funcoes javascript e modulos

---
+++ js/controllers/controlador-formulario.js
@@ -0,0 +1,11 @@
+// controladores em MCV são os módulos, componentes que tratam os eventos do usuário
+
+export function init() {
+
+    // função responsável por iniciar o controlador
+
+    const inputCep = document.forms.enderecoNovo.cep;
+    console.log(inputCep);
+
+
+}
---
+++ js/viacep.js
@@ -0,0 +1,3 @@
+import * as controladorFormulario from './controllers/controlador-formulario.js';
+
+controladorFormulario.init();
