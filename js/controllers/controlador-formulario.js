e1b5fc592c6ce97b2be814a8a515550f9076c3df
Rafael De Luca <deluca1712@gmail.com>
2022-12-16 14:20:53 -0300

    Tratando a exceção em caso de informar um CEP inválido

--- js/controllers/controlador-formulario.js
+++ js/controllers/controlador-formulario.js
@@ -5,12 +5,12 @@ import * as servicoEndereco from '../services/servico-endereco.js'
 import Endereco from '../models/endereco.js';
 
 //função construtora
-function State () {
-    
+function State() {
+
     this.botaoSalvar = null;
     this.botaoLimpar = null;
 
-    this.inputCep= null;
+    this.inputCep = null;
     this.inputLogradouro = null;
     this.inputNumero = null;
     this.inputCidade = null;
@@ -18,14 +18,11 @@ function State () {
     this.erroCep = null;
     this.erroNumero = null;
 
-    this.endereco = new Endereco ();
+    this.endereco = new Endereco();
 }
 
 // instanciando um objeto State
 const state = new State();
-
-
-
 
 export function init() {
 
@@ -45,39 +42,39 @@ export function init() {
     state.erroCep = document.querySelector('[data-erro="cep"]');
     state.erroNumero = document.querySelector('[data-erro="numero"]');
 
-   // console.log(state);
+    // console.log(state);
 
-   //setErroFormulario("cep", "testando mensagem de erro do campo cep");
-   //setErroFormulario("numero", "testando mesagem de erro do campo numero");
-   state.inputNumero.addEventListener('change', handleInputNumeroChange);
-   state.botaoLimpar.addEventListener('click',handleBotaoLimparClick);
-   state.botaoSalvar.addEventListener('click',handleBotaoSalvarClick);
-   state.inputCep.addEventListener('change', handleInputCepChange);
+    //setErroFormulario("cep", "testando mensagem de erro do campo cep");
+    //setErroFormulario("numero", "testando mesagem de erro do campo numero");
+    state.inputNumero.addEventListener('change', handleInputNumeroChange);
+    state.botaoLimpar.addEventListener('click', handleBotaoLimparClick);
+    state.botaoSalvar.addEventListener('click', handleBotaoSalvarClick);
+    state.inputCep.addEventListener('change', handleInputCepChange);
 
-   //requisicaoServico.getJson('https://viacep.com.br/ws/90245000/json/');
+    //requisicaoServico.getJson('https://viacep.com.br/ws/90245000/json/');
 
 }
 
 function handleInputNumeroChange(evento) {
     //se o conteúdo do campo estiver string vazia colocar a mensagem de erro de campo requerido
-    if(evento.target.value == "") {
-        setErroFormulario("numero","Campo requerido");
-    } 
+    if (evento.target.value == "") {
+        setErroFormulario("numero", "Campo requerido");
+    }
     else {
-    //se o campo estiver preenchido não faz nada
-        setErroFormulario("numero","")
+        //se o campo estiver preenchido não faz nada
+        setErroFormulario("numero", "")
     }
 
 }
 
-function limparFormulario () {
+function limparFormulario() {
     state.inputCep.value = "";
     state.inputLogradouro.value = "";
     state.inputNumero.value = "";
     state.inputCidade.value = "";
 
-    setErroFormulario("cep","");
-    setErroFormulario("numero","");
+    setErroFormulario("cep", "");
+    setErroFormulario("numero", "");
 
     //jogar o cursos para o campo cep
     state.inputCep.focus();
@@ -97,20 +94,39 @@ async function handleBotaoSalvarClick(evento) {
     console.log(evento.target);
 }
 
-async function handleInputCepChange (evento) {
+async function handleInputCepChange(evento) {
     const cep = evento.target.value;
-    // fazer a requisição quando houver modificação no input do cep
-    // modificado await só pode ser usado em função assincrona
-    const endereco = await servicoEndereco.buscaPorCep(cep);
-   console.log(endereco);
-   // console.log("mudou");
+
+    try {
+        // fazer a requisição quando houver modificação no input do cep
+        // modificado await só pode ser usado em função assincrona
+        const endereco = await servicoEndereco.buscaPorCep(cep);
+
+        // jogando oo campos logradouro e cidade para o formulario
+        state.inputLogradouro.value = endereco.logradouro;
+        state.inputCidade.value = endereco.cidade;
+        state.endereco = endereco;
+
+        //retirar a mensagem de erro de tivesse digitado um dep errado e jogar o foco pro input numero
+        setErroFormulario("cep", "");
+        state.inputNumero.focus();
+        //console.log(endereco);  
+
+    } catch (erro) {
+        state.inputLogradouro.value = "";
+        state.inputCidade.value = "";
+        setErroFormulario("cep", "Cep inválido. Informe um cep correto!");
+        state.inputCep.focus();
+       
+    }
+    
 
 }
 
-function setErroFormulario (chave, valor) {
+function setErroFormulario(chave, valor) {
     //verifica se o armento passado como chave é um cep ou um numero
     const elemento = document.querySelector(`[data-erro=${chave}]`);
     // cria o elemento de campo requerido no campo em branco
     elemento.innerHTML = valor;
-    
+
 }
