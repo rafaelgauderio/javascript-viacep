fc5d8dc71931753b470b1750f1080a4812e5ed79
Rafael De Luca <deluca1712@gmail.com>
2022-12-17 17:58:18 -0300

    Funcao fechar o caixa moda ao clicar fora dela

--- js/controllers/controller-modal.js
+++ js/controllers/controller-modal.js
@@ -11,6 +11,7 @@ export function iniciar () {
     state.botaoFechar = document.getElementById("fechar-caixa-modal");
 
     state.botaoFechar.addEventListener('click',handleBotaoFecharClick);
+    state.container.addEventListener('click',handleContainerClick);
 }
 
 export function mostrarModal () {
@@ -25,3 +26,15 @@ function handleBotaoFecharClick (evento) {
     evento.preventDefault();
     fecharModal();
 }
+
+function handleContainerClick(evento) {
+    evento.preventDefault();
+    if (evento.target ===this) {
+        fecharModal();
+    }
+    console.log(evento.target);
+    console.log(this);
+    //this referência ao elemento onde foi escrito o evento. no caso foi ao container(section)
+}
+
+
